ca73205c60e253cfbd2ff6bc37705116194844c2
Aashna-Verma <aashna.verma@outlook.com>
2024-10-19 13:23:32 -0400

    changed banner

--- client/src/layouts/Central/Banner/Banner.tsx
+++ client/src/layouts/Central/Banner/Banner.tsx
@@ -3,9 +3,9 @@ import "./Banner.style.scss";
 function Banner() {
   return (
     <div className="Banner">
-      <img src="/carleton_logo.png" alt="logo" />
+      <img src="/vite.svg" alt="logo" />
       <div className="Banner__seperator"></div>
-      <div className="Banner__title">Carleton Central</div>
+      <div className="Banner__title">Gorilla Gorlies Central</div>
     </div>
   );
 }
