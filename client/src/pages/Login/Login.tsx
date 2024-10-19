d582a9aad74450f198a4580905801bf5947bfc22
selenaquang <selenaquang@gmail.com>
2024-10-19 16:03:19 -0400

    Gorilla welcoming you to sign in!

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -30,36 +30,44 @@ function Login() {
   return (
     <Layout>
       <div className="Login">
-      <div className="Login__panel">
-        <div className="Login__panel__content">
-          <img src="/carleton_logo_black.png"></img>
-          <div className="Login__panel__content__message">
-            <div>Welcome to the Carleton SSO Federated Portal.</div>
-            <div>
-              Enter your{" "}
-              <a href="https://myone.carleton.ca" target="blank">
-                MyCarletonOne
-              </a>{" "}
-              username and password.
+        <div className="Login__panel">
+          <div className="Login__panel__content">
+            <img src="/carleton_logo_black.png"></img>
+            <div className="Login__panel__content__message">
+              <div>Welcome to the Carleton SSO Federated Portal.</div>
+              <div>
+                Enter your{" "}
+                <a href="https://myone.carleton.ca" target="blank">
+                  MyCarletonOne
+                </a>{" "}
+                username and password.
+              </div>
+            </div>
+            {message && <p>{message}</p>}
+            <div className="Login__panel__content__input">
+              <input type="text" placeholder="MyCarletonOne username" onChange={handleUsernameChange}></input>
+              <input type="password" placeholder="Password" onChange={handlePasswordChange}></input>
+            </div>
+            <div className="Login__panel__content__checkbox">
+              <input type="checkbox"></input>
+              <label>Keep me signed in</label>
+            </div>
+            <div className="SignInBlock">
+              {/* <div className="SignInAnimation"> */}
+              <button
+                className="Login__panel__button"
+                id="signInButton"
+                onClick={() => attemptLogin()}
+              >
+                Sign In
+              </button>
+              {/* </div> */}
+              <div className="SignInAnimation">
+                <img id="gorillaBob" src="gorillaBob.png" alt="Gorilla Bob"></img>
+              </div>
             </div>
           </div>
-          {message && <p>{message}</p>}
-          <div className="Login__panel__content__input">
-            <input type="text" placeholder="MyCarletonOne username" onChange={handleUsernameChange}></input>
-            <input type="password" placeholder="Password" onChange={handlePasswordChange}></input>
-          </div>
-          <div className="Login__panel__content__checkbox">
-            <input type="checkbox"></input>
-            <label>Keep me signed in</label>
-          </div>
-          <button
-            className="Login__panel__button"
-            onClick={() => attemptLogin()}
-          >
-            Sign In
-          </button>
-        </div>
-      </div></div>
+        </div></div>
     </Layout>
   );
 }
